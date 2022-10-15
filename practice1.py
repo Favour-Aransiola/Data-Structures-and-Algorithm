f7863b5a2a95cd8554dc76fcedbe78cf97eee987
favour <aransiolafavour@yahoo.com>
2022-10-15 03:49:00 +0100

    power of a number

--- practice1.py
+++ practice1.py
@@ -8,4 +8,15 @@ def sumOfDigit(digit):
         return 0
     return digit%10 + sumOfDigit(digit//10)
 
-print(sumOfDigit(349))
+# print(sumOfDigit(349))
+
+'''
+Calculate the Power of a number using recursion
+
+'''
+
+def powerOfANumber(number,exponent):
+    if exponent==0:
+        return 1
+    return number* powerOfANumber(number, exponent-1)
+print(powerOfANumber(5,4))
